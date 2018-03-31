79a70f845f37e74185e39acfeb90177139e2cda5
mittalprince <princemittal766@gmail.com>
2018-04-01 01:14:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Basic-Socket.io
+A simple example to learn basic socket.io
+
+```sh
+$ npm install
+```
+
+```sh
+$ npm start
+```
---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var socket = require('socket.io')
+
+var port = process.env.PORT || 8000
+var app = express()
+
+app.use(express.static('./public'))
+
+var server = app.listen(port,()=>{
+    console.log(`Server starts on port ${port}`)
+})
+
+var io =socket(server)
+
+io.on('connection',(socket)=>{
+    console.log('Made socket connection ',socket.id)
+
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat', data)
+    })
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',data)
+    })
+})
