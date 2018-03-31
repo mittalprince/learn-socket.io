5136382d830a9adac5e897a24eeb2c893b42ea2f
Prince Mittal <princemittal766@gmail.com>
2018-04-01 01:43:09 +0530

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8000')
+var socket = io.connect('https://young-shore-77603.herokuapp.com/')
 
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
